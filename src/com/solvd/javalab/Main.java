24f6603c6787f5ce8c4b8e285ce96c262abf4ede
Neuromancer <muslimbairamov@gmail.com>
2019-07-20 16:26:09 +0300

    Added ConnectionPool, connect to DB Airports

--- src/com/solvd/javalab/Main.java
+++ src/com/solvd/javalab/Main.java
@@ -2,13 +2,58 @@ package com.solvd.javalab;
 
 import org.apache.log4j.Logger;
 
+import java.sql.*;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import com.solvd.javalab.ConnectionPool.ConnectionPool;
+
 public class Main {
 
     private static final Logger log = Logger.getLogger(Main.class);
 
-    public static void main (String[] args) {
+    public static void main (String[] args) throws SQLException, ExecutionException, InterruptedException {
 
     log.info("First log message");
+
+        //create pool connection
+        ConnectionPool connectionPool = new ConnectionPool(
+                "com.mysql.jdbc.Driver",
+                "jdbc:mysql://localhost:3306/?useSSL=false", "Neuromancer", "Neuromancer",9
+        );
+
+        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+
+            List<Connection> connections = new ArrayList<>();
+
+            try {
+
+                for (int i = 0;i < 10;i++){
+                    connections.add(connectionPool.getConnection());
+                    log.info("Connection added to DB = " + i);
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                log.info("SQLException message");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                log.info("InterruptedException");
+            }
+
+
+
+        });
+
+        future.get();
+
+        log.info(future.isDone());
+        log.info(future.isCancelled());
+        log.info(future.toCompletableFuture());
+        log.info(future.toString());
 
     }
 
