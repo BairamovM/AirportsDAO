a2c9f3b6e42933bd17bdeac7078976e9cfa08c71
Neuromancer <muslimbairamov@gmail.com>
2019-07-22 17:11:07 +0300

    Added AirportClassesDAO

---
+++ src/com/solvd/javalab/dao/daoClass/AirportClassesDAO.java
@@ -0,0 +1,255 @@
+package com.solvd.javalab.dao.daoClass;
+
+import com.solvd.javalab.dao.entity.AirportClasses;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+public class AirportClassesDAO implements DAO <AirportClasses> {
+
+    private static final Logger logDAO = Logger.getLogger(AirportClassesDAO.class);
+
+    public static final String getId = "SELECT * FROM AirportClasses WHERE id = ?";
+    public static final String getAll = "SELECT * FROM AirportClasses";
+    public static final String save = "INSERT INTO AirportClasses VALUES (?,?,?,?)";
+    public static final String delete = "DELETE FROM AirportClasses WHERE id =?";
+    public static final String update = "UPDATE AirportClasses SET classType = ?, WHERE id =?";
+
+    @Override
+    public AirportClasses getByID(Long id) {
+
+        logDAO.trace("Try to find AirportClasses with id = " + id);
+
+        AirportClasses airportClasses = null;
+        Connection connection = null;
+        PreparedStatement prStatement = null;
+        ResultSet rSet = null;
+
+        try {
+            logDAO.trace("Open Connection");
+            connection = ConnectionFactory.getConnection();
+            try {
+                logDAO.trace("Create PreparedStatement");
+                prStatement = connection.prepareStatement(getId);
+                prStatement.setLong(1, id);
+                try {
+                    logDAO.trace("Get ResultSet");
+                    rSet = prStatement.executeQuery();
+                    if (rSet.next()) {
+                        airportClasses = new AirportClasses();
+                        airportClasses.setId(rSet.getLong("id"));
+                        airportClasses.setClassType(rSet.getLong("classType"));
+                        return airportClasses;
+                    }
+                } finally {
+                    try {
+                        rSet.close();
+                        logDAO.trace("ResultSet closed");
+                    } catch (SQLException e) {
+                        logDAO.warn("Can't close ResultSet", e);
+                    }
+                }
+            } finally {
+                try {
+                    prStatement.close();
+                    logDAO.trace("PreparedStatement closed");
+                } catch (SQLException e) {
+                    logDAO.warn("Can't close PreparedStatement", e);
+                }
+            }
+        } catch (SQLException e) {
+            logDAO.trace("Can't find AirportClasses");
+        } finally {
+            try {
+                connection.close();
+                logDAO.trace("Connection closed");
+            } catch (SQLException e) {
+                logDAO.warn("Can't close Connection", e);
+            }
+        }
+
+        if (null == airportClasses) {
+            logDAO.debug("airportClasses not found");
+        } else {
+            logDAO.trace("Find airportClasses");
+        }
+
+        logDAO.trace("Return airportClasses");
+        return airportClasses;
+    }
+
+    @Override
+    public List<AirportClasses> getAll() {
+
+        logDAO.trace("Try to find all Airplanes");
+        AirportClasses airportClasses = null;
+        Connection connection = null;
+        PreparedStatement prStatement = null;
+        ResultSet rSet = null;
+
+        try {
+            logDAO.trace("Open Connection");
+            connection = ConnectionFactory.getConnection();
+            try {
+                logDAO.trace("Create PreparedStatement");
+                prStatement = connection.prepareStatement(getAll);
+                try {
+                    logDAO.trace("Get ResultSet");
+                    rSet = prStatement.executeQuery();
+                    List<AirportClasses> airportClass = new ArrayList<>();
+                    while (rSet.next()) {
+                        airportClasses = new AirportClasses();
+                        airportClasses.setId(rSet.getLong("id"));
+                        airportClasses.setClassType(rSet.getLong("classType"));
+                        airportClass.add(airportClasses);
+
+                    }
+                    return airportClass;
+                } finally {
+                    try {
+                        rSet.close();
+                        logDAO.trace("ResultSet closed");
+                    } catch (SQLException e) {
+                        logDAO.warn("Can't close ResultSet", e);
+                    }
+                }
+            } finally {
+                try {
+                    prStatement.close();
+                    logDAO.trace("PreparedStatement closed");
+                } catch (SQLException e) {
+                    logDAO.warn("Can't close PreparedStatement", e);
+                }
+            }
+        } catch (SQLException e) {
+            logDAO.trace("Can't find any airportClasses", e);
+        } finally {
+            try {
+                connection.close();
+                logDAO.trace("Connection closed");
+            } catch (SQLException e) {
+                logDAO.warn("Can't close Connection", e);
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public void save(AirportClasses airportClasses) {
+
+        logDAO.trace("Update Data in airportClasses");
+        Connection connection = null;
+        PreparedStatement prStatement = null;
+
+        try {
+            logDAO.trace("Open Connection");
+            connection = ConnectionFactory.getConnection();
+            try {
+                logDAO.trace("Create PreparedStatement");
+                prStatement = connection.prepareStatement(save);
+
+                prStatement.setLong(1, airportClasses.getClassType());
+
+                prStatement.executeUpdate();
+            } finally {
+                try {
+                    prStatement.close();
+                    logDAO.trace("PreparedStatement closed");
+                } catch (SQLException e) {
+                    logDAO.warn("Can't close PreparedStatement", e);
+                }
+            }
+        } catch (SQLException e) {
+            logDAO.trace("Can't Updata Data in airportClasses", e);
+        } finally {
+            try {
+                connection.close();
+                logDAO.trace("Connection closed");
+            } catch (SQLException e) {
+                logDAO.warn("Can't close Connection", e);
+            }
+        }
+
+    }
+
+    @Override
+    public void delete(Long id) {
+
+        logDAO.trace("Delete Data from airportClasses");
+        Connection connection = null;
+        PreparedStatement prStatement = null;
+
+        try {
+            logDAO.trace("Open Connection");
+            connection = ConnectionFactory.getConnection();
+            try {
+                logDAO.trace("Create PreparedStatement");
+                prStatement = connection.prepareStatement(delete);
+                prStatement.setLong(1, id);
+                prStatement.executeUpdate(delete);
+            } finally {
+                try {
+                    prStatement.close();
+                    logDAO.trace("PreparedStatement closed");
+                } catch (SQLException e) {
+                    logDAO.warn("Can't close PreparedStatement", e);
+                }
+            }
+        } catch (SQLException e) {
+            logDAO.trace("Can't Delete Data in airportClasses");
+        } finally {
+            try {
+                connection.close();
+                logDAO.trace("Connection closed");
+            } catch (SQLException e) {
+                logDAO.warn("Can't close Connection", e);
+            }
+        }
+
+    }
+
+    @Override
+    public void update(AirportClasses airportClasses) {
+
+        logDAO.trace("Update Data in airportClasses");
+        Connection connection = null;
+        PreparedStatement prStatement = null;
+
+        try {
+            logDAO.trace("Open Connection");
+            connection = ConnectionFactory.getConnection();
+            try {
+                logDAO.trace("Create PreparedStatement");
+                prStatement = connection.prepareStatement(update);
+                prStatement.setLong(5, airportClasses.getClassType());
+
+                prStatement.executeUpdate();
+            } finally {
+                try {
+                    prStatement.close();
+                    logDAO.trace("PreparedStatement closed");
+                } catch (SQLException e) {
+                    logDAO.warn("Can't close PreparedStatement", e);
+                }
+            }
+        } catch (SQLException e) {
+            logDAO.trace("Can't Update Data in AirportClasses", e);
+        } finally {
+            try {
+                connection.close();
+                logDAO.trace("Connection closed");
+            } catch (SQLException e) {
+                logDAO.warn("Can't close Connection", e);
+            }
+        }
+
+    }
+
+}
