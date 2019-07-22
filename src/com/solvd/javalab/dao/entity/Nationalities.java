4a18898b3ef41b0c26b5a9c92302e899ad717d8a
Neuromancer <muslimbairamov@gmail.com>
2019-07-22 10:34:00 +0300

    Added Nationalities, logic to setFunctions with log

---
+++ src/com/solvd/javalab/dao/entity/Nationalities.java
@@ -0,0 +1,47 @@
+package com.solvd.javalab.dao.entity;
+
+import org.apache.log4j.Logger;
+
+public class Nationalities {
+
+    private static final Logger logEntity = Logger.getLogger(Nationalities.class);
+
+    private Long id;
+    private String citizenship;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+
+        try {
+            if (id <= 0) {
+                logEntity.error("Value id error = " + id);
+                throw new EntityException("Value id error = " + id);
+            } else if (id >= 1) {
+                this.id = id;
+            }
+        } catch (EntityException e) {
+            logEntity.error("Value id error = " + id);
+        }
+    }
+
+    public String getCitizenship() {
+        return citizenship;
+    }
+
+    public void setCitizenship(String citizenship) {
+
+        try {
+            if (citizenship == null) {
+                logEntity.error("Value citizenship NULL error = " + citizenship);
+                throw new EntityException("Value citizenship NULL error = " + citizenship);
+            } else if (citizenship != null) {
+                this.citizenship = citizenship;
+            }
+        } catch (EntityException e) {
+            logEntity.error("Value citizenship NULL error = " + citizenship);
+        }
+    }
+}
